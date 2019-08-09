1c9b84eb3efd7de49821e94cc2d79670ea93003d
mingquan <mingquan@qq.com>
2019-08-09 14:19:37 +0800

    logger

---
+++ doumingquan/python/module/logging/base.py
@@ -0,0 +1,14 @@
+import logging
+
+# logging.config.fileConfig("./logging.conf")
+logging.basicConfig(filename='logger.log', level=logging.INFO)
+
+# create logger
+logger_name1 = "example01"
+logger = logging.getLogger(logger_name1)
+
+logger.debug('debug message')
+logger.info('info message')
+logger.warn('warn message')
+logger.error('error message')
+logger.critical('critical message')
