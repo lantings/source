adecef9425bba7f734c8ba9077b53a9f1db250e7
mingquan <mingquan@qq.com>
2019-08-13 19:10:13 +0800

    algorithm

---
+++ doumingquan/golang/base/small_algorithm/99.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+//打印99乘法口诀
+func main(){
+	fmt.Println("this is very important")
+	for i:=1;i<=9;i++ {
+		for j:=1;j<=i;j++ {
+		  fmt.Printf("%d*%d=%d ",j,i,j*i)
+		}
+		 fmt.Println(" ")
+	}	
+}
