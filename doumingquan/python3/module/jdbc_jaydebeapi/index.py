61cfc5d75227acef576cb1d7f6614e749ed36f9e
mingquan <mingquan@qq.com>
2019-08-14 15:23:24 +0800

    jdbc

--- doumingquan/python3/module/jdbc_jaydebeapi/index.py
+++ doumingquan/python3/module/jdbc_jaydebeapi/index.py
@@ -1,5 +1,6 @@
 import jaydebeapi
 # pip install JayDeBeApi
+# py2和py3参数有区别
 # conn=jaydebeapi.connect('com.mysql.jdbc.Driver',['jdbc:mysql://localhost:3306/test','hive','hive'],'/data/users/huser/lan/codes/useJPype/mysql-connector-java-5.1.36/mysql-connector-java-5.1.36-bin.jar')
 # 其中mysql的用户名和密码都是hive,最后一个参数是驱动的jar包
 
@@ -20,3 +21,22 @@ curs.execute("select * from CUSTOMER")
 result=curs.fetchall()
 print(result)
 curs.close()
+
+
+# import jaydebeapi  ##使用jdbc驱动连接数据库
+# import pandas as pd
+#
+# dirver = 'oracle.jdbc.driver.OracleDriver'
+# jarFile = 'D:\\WORK\\PYScript\\BiDataMonitor\\jdbc\\64\\ojdbc14.jar'
+# addr_ = 'xxx.xxx.xxx.xxx' + ':' + '1521' + '/' + 'xxx'
+# url = 'jdbc:oracle:thin:@' + addr_
+# print('url', url)
+# DBUser = '用户名'
+# DBPwd = '密码'
+# conn = jaydebeapi.connect(dirver, [url, DBUser, DBPwd], jarFile)  ##使用jdbc驱动连接数据库
+# sql_str = "select 'oracle' from dual"
+# df = pd.read_sql_query(sql_str, conn)
+# print(df)
+# conn.close()
+
+# https: // blog.csdn.net / cakecc2008 / article / details / 79786378
