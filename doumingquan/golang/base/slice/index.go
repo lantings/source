2bfa32a678ee8b2875ea69879467c3b8d866a840
mingquan <mingquan@qq.com>
2019-08-15 17:31:18 +0800

    slice

---
+++ doumingquan/golang/base/slice/index.go
@@ -0,0 +1,37 @@
+package main
+import "fmt"
+
+func main(){
+//make定义长度为3的数组***默认值是3个nil***
+var numbers = make([]int,6,6) 
+//赋值
+numbers = []int{4,24,99,46,29,55}
+//定义并赋值
+letters := []string{"a", "b", "c", "d"}
+//对数组进行追加
+a:= append(numbers, 1, 2, 3,5,8,13)
+letters = append(letters,"f","mingquan","redis")
+
+lets := []string{"test", "edu", "cn", "slice"}
+//合并
+lets = append(letters,lets...)
+
+ /* 打印子切片从索引 2(包含) 到索引 5(不包含) */
+number3 := numbers[2:5]
+   printInt(numbers)
+   fmt.Println("numbers[1:4] ==", numbers[1:4])
+   fmt.Println("numbers[2:5] ==", number3)
+   printSlice(letters)
+   printInt(a)
+   printSlice(lets)
+   
+}
+
+
+func printSlice(x []string){
+   fmt.Printf("len=%d cap=%d slice=%v\n",len(x),cap(x),x)
+}
+
+func printInt(x []int){
+   fmt.Printf("len=%d cap=%d slice=%v\n",len(x),cap(x),x)
+}
