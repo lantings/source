9e3b66d6fcea155c3b3632287800e2ae7f4bc84c
mingquan <mingquan@qq.com>
2019-12-05 11:26:41 +0800

    monkey_king

--- doumingquan/php/algorithm/monkey_king/king.php
+++ doumingquan/php/algorithm/monkey_king/king.php
@@ -1,17 +1,25 @@
 <?php 
-// $n猴子总数，$m 数字
-function ($m,$n){
-	$monkey = range(1, $n);
+/*
+有n只猴子 数字为m
+思路：有n只猴子，当数到m的时候那只猴子就T掉，不等于的话就把这个数据加到末尾在把原来那个位置给T掉
+*/
+function getMonkeyKing($m,$n){
+	$monkeys = range(1,$n);
 	$i=0;
-	while(count($monkey)>1){
+	while(count($monkeys)>1){
 		if(($i+1)%$m==0){
-			unset($monkey[$i]);
+			unset($monkeys[$i]);
 		}else{
-			array_push($monkey, $monkey[$i]);//把没有被数到的猴子放到末尾
-			unset($monkey[$i]);
+			array_push($monkeys, $monkeys[$i]);//把没有被数到的猴子放到末尾
+			unset($monkeys[$i]);
 		}
-		$i++;
+	$i++;
 	}
-	return current($monkey);
+	return current($monkeys);
+
 }
- ?>
+$result = getMonkeyKing(11,10);
+// var_dump($result);
+
+
+?>
